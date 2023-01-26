e168da397bb314a79d02a09215d6643296228883
edge <edgedlt@protonmail.com@users.noreply.github.com>
2023-01-26 20:58:14 +0000

    Tidy up a bit.

--- shrike-api/src/stat/internals.rs
+++ shrike-api/src/stat/internals.rs
@@ -3,59 +3,48 @@ use r2d2_sqlite::SqliteConnectionManager;
 
 const PRECISION: f64 = 100000000.0;
 
-pub fn get_blocks_internal(conn: &PooledConnection<SqliteConnectionManager>) -> u64 {
-    let sql = "SELECT id FROM blocks WHERE id=(SELECT max(id) FROM blocks)";
+// Should be possible to make this generic but the way to do it in this context escapes me currently
+pub fn get_unsigned_stat_internal(conn: &PooledConnection<SqliteConnectionManager>, sql: &str) -> u64 {
     let mut stmt = conn.prepare(sql).unwrap();
-
     let total: Result<u64, rusqlite::Error> = stmt.query_row([], |row| row.get(0));
 
     total.unwrap()
+}
+
+pub fn get_floating_stat_internal(conn: &PooledConnection<SqliteConnectionManager>, sql: &str) -> f64 {
+    let mut stmt = conn.prepare(sql).unwrap();
+    let total: Result<f64, rusqlite::Error> = stmt.query_row([], |row| row.get(0));
+
+    total.unwrap()
+}
+
+pub fn get_blocks_internal(conn: &PooledConnection<SqliteConnectionManager>) -> u64 {
+    let sql = "SELECT id FROM blocks WHERE id=(SELECT max(id) FROM blocks)";
+    get_unsigned_stat_internal(conn, sql)
 }
 
 pub fn get_transactions_internal(conn: &PooledConnection<SqliteConnectionManager>) -> u64 {
     let sql = "SELECT id FROM transactions WHERE id=(SELECT max(id) FROM transactions)";
-    let mut stmt = conn.prepare(sql).unwrap();
-
-    let total: Result<u64, rusqlite::Error> = stmt.query_row([], |row| row.get(0));
-
-    total.unwrap()
+    get_unsigned_stat_internal(conn, sql)
 }
 
 pub fn get_sysfee_internal(conn: &PooledConnection<SqliteConnectionManager>) -> f64 {
     let sql = "SELECT sum(sysfee) FROM transactions";
-    let mut stmt = conn.prepare(sql).unwrap();
-
-    let res: Result<u64, rusqlite::Error> = stmt.query_row([], |row| row.get(0));
-    let total = res.unwrap() as f64 / PRECISION;
-
-    total
+    get_floating_stat_internal(conn, sql) / PRECISION
 }
 
 pub fn get_transfers_internal(conn: &PooledConnection<SqliteConnectionManager>) -> u64 {
     let sql = "SELECT SUM(LENGTH(notifications) - LENGTH(REPLACE(notifications, 'Transfer', ''))) / 8 FROM transactions WHERE notifications LIKE '%Transfer%'";
-    let mut stmt = conn.prepare(sql).unwrap();
-
-    let total: Result<u64, rusqlite::Error> = stmt.query_row([], |row| row.get(0));
-
-    total.unwrap()
+    get_unsigned_stat_internal(conn, sql)
 }
 
 pub fn get_senders_internal(conn: &PooledConnection<SqliteConnectionManager>) -> u64 {
     let sql = "SELECT COUNT(DISTINCT sender) FROM transactions";
-    let mut stmt = conn.prepare(&sql).unwrap();
-
-    let senders: Result<u64, rusqlite::Error> = stmt.query_row([], |row| row.get(0));
-
-    senders.unwrap()
+    get_unsigned_stat_internal(conn, sql)
 }
 
 pub fn get_contracts_internal(conn: &PooledConnection<SqliteConnectionManager>) -> u64 {
     let deploy_event = r#"'%"contract":"0xfffdc93764dbaddd97c48f252a53ea4643faa3fd","eventname":"Deploy"%'"#;
-
     let sql = "SELECT COUNT() FROM transactions WHERE notifications LIKE ".to_string() + deploy_event;
-    let mut stmt = conn.prepare(&sql).unwrap();
-
-    let contracts: Result<u64, rusqlite::Error> = stmt.query_row([], |row| row.get(0));
-
-    contracts.unwrap() + 9 // fetch natives properly in future
+    get_unsigned_stat_internal(conn, &sql) + 9 // fetch natives properly in future
 }
